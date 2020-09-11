a7acaec67b36e002c619d1eb5acbcba5367f0f98
ThanmayJ <thanmay.jayakumar@gmail.com>
2020-09-11 16:25:46 +0530

    Edited Feature-Match-LBG and added a few comments

--- Feature-Match-LBG.py
+++ Feature-Match-LBG.py
@@ -26,23 +26,24 @@ def lbg_codebook(fvs, M):
   """
   
   #INITIALIZATION
-  no_centroids = 1 #No. of codevectors is 1 intially, so only one centroid
-  codebook = np.mean(fvs, axis = 0) #Codebook is the Centroid of features
-  e = 0.01  #Splitting factor e = 0.01
+  centroids = 1
+  centroid_coord = np.mean(fvs, axis = 0) #Centroid of features
+  r = centroids   #number of rows in codebook (initially)
+  c = len(centroid_coord)   #number of columns in codebook (initially)
+  codebook = np.empty((r,c))  #codebook created
+  codebook[0] = centroid_coord #first and only codevector will be the centroid of the given features
+
+  e = 0.01
   distortion = 1
-  r = no_centroids   #number of rows in codebook (initially)
-  c = len(codebook)   #number of columns in codebook (initially)
   
   #DOUBLING THE CODEBOOK according to the formula y(n)+ = y(n)*(1+e) , y(n)- = y(n)*(1-e)
   while no_centroids < M: 
     newcodebook = np.empty((2*r, c)) #Creating a temporary codebook that is to be updated
-    if no_centroids == 1:
-            newcodebook[0] = codebook*(1+e)
-            newcodebook[1] = codebook*(1-e)
-    else:
-      for i in range(no_centroids):
-        newcodebook[2*i] = codebook[i] * (1+e)    #y(n)+ = y(n)*(1+e)
-        newcodebook[2*i+1] = codebook[i] * (1-e)  #y(n)- = y(n)*(1-e) 
+
+    for i in range(no_centroids):
+      newcodebook[2*i] = codebook[i] * (1+e)    #y(n)+ = y(n)*(1+e)
+      newcodebook[2*i+1] = codebook[i] * (1-e)  #y(n)- = y(n)*(1-e)
+
 
     codebook = newcodebook #codebook updated
     r = np.shape(codebook)[0] #updating the value of centroid // again, the number of centroids = number of codevectors
